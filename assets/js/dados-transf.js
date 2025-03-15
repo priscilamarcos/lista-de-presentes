763115ed341a9013e7ad65749fa6a547fc7017fa
depontoaponto <marcos.orelio.vianna@gmail.com>
2025-03-14 21:49:33 -0300

    token cors fix

---
+++ assets/js/dados-transf.js
@@ -0,0 +1,132 @@
+export function getDadosJson() {
+    return _dadosJson;
+}
+
+const _dadosJson = [{
+
+    "id": "1",
+    "imgFile": "./assets/img/lista-idosa.jpeg",
+    "titulo": "Titulo Item1",
+    "descrição": "Descrição",
+    "valor": "10,01",
+    "qrCode": "http://wwwrrrgggbbb.com",
+    "ativo": "true"
+},
+{
+    "id": "2",
+    "imgFile": "./assets/img/lista-idosa.jpeg",
+    "titulo": "Titulo Item1",
+    "descrição": "Descrição",
+    "valor": "10,01",
+    "qrCode": "http://wwwrrrgggbbb.com",
+    "ativo": "true"
+},
+{
+    "id": "3",
+    "imgFile": "./assets/img/lista-idosa.jpeg",
+    "titulo": "Titulo Item1",
+    "descrição": "Descrição",
+    "valor": "10,01",
+    "qrCode": "http://wwwrrrgggbbb.com",
+    "ativo": "true"
+},
+{
+    "id": "4",
+    "imgFile": "./assets/img/lista-idosa.jpeg",
+    "titulo": "Titulo Item1",
+    "descrição": "Descrição",
+    "valor": "10,01",
+    "qrCode": "http://wwwrrrgggbbb.com",
+    "ativo": "true"
+},
+{
+    "id": "5",
+    "imgFile": "./assets/img/lista-idosa.jpeg",
+    "titulo": "Titulo Item1",
+    "descrição": "Descrição",
+    "valor": "10,01",
+    "qrCode": "http://wwwrrrgggbbb.com",
+    "ativo": "true"
+},
+{
+    "id": "6",
+    "imgFile": "./assets/img/lista-idosa.jpeg",
+    "titulo": "Titulo Item1",
+    "descrição": "Descrição",
+    "valor": "10,01",
+    "qrCode": "http://wwwrrrgggbbb.com",
+    "ativo": "true"
+},
+{
+    "id": "7",
+    "imgFile": "./assets/img/lista-idosa.jpeg",
+    "titulo": "Titulo Item1",
+    "descrição": "Descrição",
+    "valor": "10,01",
+    "qrCode": "http://wwwrrrgggbbb.com",
+    "ativo": "true"
+},
+{
+    "id": "8",
+    "imgFile": "./assets/img/lista-idosa.jpeg",
+    "titulo": "Titulo Item1",
+    "descrição": "Descrição",
+    "valor": "10,01",
+    "qrCode": "http://wwwrrrgggbbb.com",
+    "ativo": "true"
+},
+{
+    "id": "9",
+    "imgFile": "./assets/img/lista-idosa.jpeg",
+    "titulo": "Titulo Item1",
+    "descrição": "Descrição",
+    "valor": "10,01",
+    "qrCode": "http://wwwrrrgggbbb.com",
+    "ativo": "true"
+},
+{
+    "id": "10",
+    "imgFile": "./assets/img/lista-idosa.jpeg",
+    "titulo": "Titulo Item1",
+    "descrição": "Descrição",
+    "valor": "10,01",
+    "qrCode": "http://wwwrrrgggbbb.com",
+    "ativo": "true"
+},
+{
+    "id": "11",
+    "imgFile": "./assets/img/lista-idosa.jpeg",
+    "titulo": "Titulo Item1",
+    "descrição": "Descrição",
+    "valor": "10,01",
+    "qrCode": "http://wwwrrrgggbbb.com",
+    "ativo": "true"
+},
+{
+    "id": "12",
+    "imgFile": "./assets/img/lista-idosa.jpeg",
+    "titulo": "Titulo Item1",
+    "descrição": "Descrição",
+    "valor": "10,01",
+    "qrCode": "http://wwwrrrgggbbb.com",
+    "ativo": "true"
+},
+{
+    "id": "13",
+    "imgFile": "./assets/img/lista-idosa.jpeg",
+    "titulo": "Titulo Item1",
+    "descrição": "Descrição",
+    "valor": "10,01",
+    "qrCode": "http://wwwrrrgggbbb.com",
+    "ativo": "true"
+},
+{
+    "id": "14",
+    "imgFile": "./assets/img/lista-idosa.jpeg",
+    "titulo": "Titulo Item1",
+    "descrição": "Descrição",
+    "valor": "10,01",
+    "qrCode": "http://wwwrrrgggbbb.com",
+    "ativo": "true"
+}]
+
